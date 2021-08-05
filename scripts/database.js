6e191bf2064b12e79d177c138f85bcf74662791d
Blake Rhoden <zbrhoden@gmail.com>
2021-08-05 10:42:56 -0500

    edited the database

--- scripts/database.js
+++ scripts/database.js
@@ -1,10 +1,20 @@
 const database = {
     colonies: [
-
+        {id: 1, name: "Roanoke", inventory: 1},
+        {id: 2, name: "Earfbase", inventory: 8},
+        {id: 3, name: "Asteroidopolis", inventory: 2},
+        {id: 4, name: "Krypton", inventory: 6},
+        {id: 5, name: "Deimos Moon Base", inventory: 3},
     ],
+    
     minerals: [
-
+        {id: 1, type: "Adamantium", price: 9000},
+        {id: 2, type: "Vibranium", price: 6000},
+        {id: 3, type: "Kryptonite", price:2000},
+        {id: 4, type: "Osmium", price: 4000},
+        {id: 5, type: "Moon Dust", price: 1000, }
     ],
+
     governors: [
         { id: 1, name: "David Davis", active: true, colonyId: 1  },
         { id: 2, name: "Mitch Brown", active: true, colonyId: 1 },
@@ -17,6 +27,7 @@ const database = {
         { id: 9, name: "Darcy Martin", active: true, colonyId: 7  },
         { id: 10, name: "Mason Stone", active: true, colonyId: 8  },
     ],
+
     facilities: [
         { id: 1, name: "Callisto", active: true },
         { id: 2, name: "Europa", active: true },
@@ -26,6 +37,7 @@ const database = {
         { id: 6, name: "Charon", active: true },
         { id: 7, name: "Ariel", active: true },
     ],
+    
     orderBuilder: {
     }
 }

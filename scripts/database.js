bb3f1d5ea26131a4ff5401e8ed6f6c1f51bef223
Alexander Schroyer <alexanderschroyer@gmail.com>
2021-08-05 10:18:37 -0500

    colinies and minerals populated in the database

--- scripts/database.js
+++ scripts/database.js
@@ -1,21 +1,24 @@
 const database = {
-    colonies: [{
-
-    }
-],
+    colonies: [
+        {id: 1, name: "Roanoke", inventory: 1},
+        {id: 2, name: "Earfbase", inventory: 8},
+        {id: 3, name: "Asteroidopolis", inventory: 2},
+        {id: 4, name: "Krypton", inventory: 6},
+        {id: 5, name: "Deimos Moon Base", inventory: 3},
+    ],
     governors: [
-        {
-
-    }
-],
-    facilities: [{
-
-    }
-],
-    minerals: [{
-
-    }
-]
+        {}
+    ],
+    facilities: [
+        {}
+    ],
+    minerals: [
+        {id: 1, type: "Adamantium", price: 9000},
+        {id: 2, type: "Vibranium", price: 6000},
+        {id: 3, type: "Kryptonite", price:2000},
+        {id: 4, type: "Osmium", price: 4000},
+        {id: 5, type: "Moon Dust", price: 1000, }
+    ]
 }
 
 

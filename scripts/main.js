d6de542fafb09003b90b2db9f10bcbb969debfd8
Alexander Schroyer <alexanderschroyer@gmail.com>
2021-08-05 11:02:55 -0500

    main render addition pre-import

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+
+
+
+const mainContainer = document.querySelector("#container")
+
+const renderAllHTML = () => {
+    mainContainer.innerHTML = willYouBeMine()
+}
+
+renderAllHTML()
+
+document.addEventListener("stateChanged", event => {
+    console.log("State of data has changed. Regenerating HTML...")
+    renderAllHTML()
+})

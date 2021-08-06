e626c19d42d3f061b753e716cbb8924b1a384058
Alexander Schroyer <alexanderschroyer@gmail.com>
2021-08-06 10:22:55 -0500

    added governor active dropdown with non-active grayed out

--- scripts/Governors.js
+++ scripts/Governors.js
@@ -4,7 +4,7 @@ import { getGovernors, setGovernor } from "./database.js"
 const governor = getGovernors()
 
 
-<button onclick="myFunction()">Click me</button>
+{/* <button onclick="myFunction()">Click me</button> */}
 // this code breaks it but we are using it in a minute.
 
 
@@ -19,8 +19,14 @@ export const Governors = () => {
     let html = "<select>"
 
     const listItemsArray = governor.map(governor => {
-        return `<option input type="checkbox" name="governor" value=${governor.name}/>${governor.name}
+        if (governor.active === true) {
+            return `<option input type="checkbox" name="governor" value=${governor.name}/> ${governor.name}
         </option>`
+        } else {
+            return `<option input type="checkbox" disabled="true" name="governor" value=${governor.name}/> ${governor.name}
+            </option>`
+        }
+        // we added the disabled class feature to the else statement so the names remained grayed.
     })
 
     html += listItemsArray.join("")
--- scripts/database.js
+++ scripts/database.js
@@ -8,14 +8,14 @@ const database = {
     ],
   
     governors: [
-        { id: 1, name: "David Davis", active: true, colonyId: 1  },
+        { id: 1, name: "David Davis", active: false, colonyId: 1  },
         { id: 2, name: "Mitch Brown", active: true, colonyId: 1 },
         { id: 3, name: "Andrew Anderson", active: true, colonyId: 2  },
-        { id: 4, name: "Audrey Johnson", active: true, colonyId: 2  },
+        { id: 4, name: "Audrey Johnson", active: false, colonyId: 2  },
         { id: 5, name: "Michael Smith", active: true, colonyId: 3  },
-        { id: 6, name: "Sarah Miller", active: true, colonyId: 4  },
+        { id: 6, name: "Sarah Miller", active: false, colonyId: 4  },
         { id: 7, name: "Isabella Garcia", active: true, colonyId: 5  },
-        { id: 8, name: "Hans Weber", active: true, colonyId: 6 },
+        { id: 8, name: "Hans Weber", active: false, colonyId: 6 },
         { id: 9, name: "Darcy Martin", active: true, colonyId: 7  },
         { id: 10, name: "Mason Stone", active: true, colonyId: 8  },
     ],
@@ -23,8 +23,8 @@ const database = {
     facilities: [
         { id: 1, name: "Callisto", active: true },
         { id: 2, name: "Europa", active: true },
-        { id: 3, name: "Triton", active: true },
-        { id: 4, name: "Rhea", active: true }
+        { id: 3, name: "Triton", active: false },
+        { id: 4, name: "Rhea", active: false }
     ],
     minerals: [
         {id: 1, type: "Adamantium", price: 900, quantity: 35},
